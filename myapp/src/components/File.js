532e1ac1b4b918ddcf15f42a000d041a03387e92
montejs3 <98852422+montejs3@users.noreply.github.com>
2023-10-29 20:59:57 -0500

    Final

--- myapp/src/components/File.js
+++ myapp/src/components/File.js
@@ -5,18 +5,14 @@ import { Form , Button, Row, Container} from 'react-bootstrap';
 function File() {
     const [inputText, setInputText] = useState([]); // State to store user input
     const [predictions, setPredictions] = useState([]); // State to store model predictions
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [response, setResponse] = useState(null);
-    const [selectedFileType, setSelectedFileType] = useState();
+
 
     const handleInputChange = (event) => {
      
         setInputText(event.target.value);
     };
 
-    const handleFileTypeChange = (event) => {
-      setSelectedFileType(event.target.value);
-    };
+
   
     const handlePredictClick = async () => {
         // Make a POST request to the API endpoint
@@ -29,43 +25,9 @@ function File() {
         }
       };
 
-      const handleFileChange = (event) => {
-        setSelectedFile("dd");
-      };
+     
 
-      const handleUpload = () => {
-        if (selectedFile) {
-          console.log(selectedFile);
-          const formData = new FormData();
-          formData.append('file', selectedFile);
-          if (selectedFileType === "csv") {
-          axios.post('http://127.0.0.1:8000/uploadCsv', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then((res) => {
-            setResponse(res.data);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-        }
-        if (selectedFileType === "excel") {
-          axios.post('http://127.0.0.1:8000/uploadExcel', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then((res) => {
-            setResponse(res.data);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-        }
-        }
-      };
+  
   
     return (
     <Container>
